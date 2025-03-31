9efaefacbbe71a3567c4bec245e18bcc6c023a7e
Bijun Li <bijunli86@gmail.com>
2025-03-31 23:26:27 +0200

    Clean up legacy code

--- src/utils/chineseNumber.ts
+++
@@ -1,5 +0,0 @@
-export function toChineseNumber(num: number): string {
-  const chineseNumbers = ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九', '十'];
-  if (num < 0 || num > 10) return num.toString(); // 只处理0-10的数字
-  return chineseNumbers[num];
-} 
